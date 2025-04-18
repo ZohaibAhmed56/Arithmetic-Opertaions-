87a341870c5d4d165db3458bd1e48038d82a0df1
Zohaib Baloch <ahmed.zohaib2008@gmail.com>
2025-04-18 17:05:49 +0500

    Add files via upload

---
+++ assignment-2.php
@@ -0,0 +1,166 @@
+ <?php
+
+    echo "<h1 style= color:green; >Plus</h1>";
+    //one
+    echo 10 + 17;
+    echo "<br>";
+    //two
+    echo 50 + 50;
+    echo "<br>";
+    //three
+    echo 70 + 60;
+    echo "<br>";
+    //four
+    echo 90 + 180;
+    echo "<br>";
+    //five
+    echo 160 + 80;
+    echo "<br>";
+    //six
+    echo 200 + 700;
+    echo "<br>";
+    //seven
+    echo 500 + 900;
+    echo "<br>";
+    //eight 
+    echo 470 + 40;
+    echo "<br>";
+    //nine
+    echo 560 + 430;
+    echo "<br>";
+    //ten
+    echo 690 + 890;
+
+    echo "<br>";
+
+
+    echo "<h1 style= color:red; >Subtraction</h1>";
+    //one
+    echo 10 - 17;
+    echo "<br>";
+    //two
+    echo 50 - 50;
+    echo "<br>";
+    //three
+    echo 70 - 60;
+    echo "<br>";
+    //four
+    echo 90 - 180;
+    echo "<br>";
+    //five
+    echo 160 - 80;
+    echo "<br>";
+    //six
+    echo 200 - 700;
+    echo "<br>";
+    //seven
+    echo 500 - 900;
+    echo "<br>";
+    //eight 
+    echo 470 - 40;
+    echo "<br>";
+    //nine
+    echo 560 - 430;
+    echo "<br>";
+    //ten
+    echo 690 - 890;
+    echo "<br>";
+
+
+    echo "<h1 style= color:yellow; >Multiplication</h1>";
+    //one
+    echo 10 * 17;
+    echo "<br>";
+    //two
+    echo 50 * 50;
+    echo "<br>";
+    //three
+    echo 70 * 60;
+    echo "<br>";
+    //four
+    echo 90 * 180;
+    echo "<br>";
+    //five
+    echo 160 * 80;
+    echo "<br>";
+    //six
+    echo 200 * 700;
+    echo "<br>";
+    //seven
+    echo 500 * 900;
+    echo "<br>";
+    //eight 
+    echo 470 * 40;
+    echo "<br>";
+    //nine
+    echo 560 * 430;
+    echo "<br>";
+    //ten
+    echo 690 * 890;
+    echo "<br>";
+
+    echo "<h1 style= color:purple; >Division</h1>";
+//one
+echo 10 / 17;
+echo "<br>";
+//two
+echo 50 / 50;
+echo "<br>";
+//three
+echo 70 / 60;
+echo "<br>";
+//four
+echo 90 / 180;
+echo "<br>";
+//five
+echo 160 / 80;
+echo "<br>";
+//six
+echo 200 / 700;
+echo "<br>";
+//seven
+echo 500 / 900;
+echo "<br>";
+//eight 
+echo 470 / 40;
+echo "<br>";
+//nine
+echo 560 / 430;
+echo "<br>";
+//ten
+echo 690 / 890;
+echo "<br>";
+
+    
+echo "<h1 style= color:orange; >Modulus</h1>";
+//one
+echo 10 % 17;
+echo "<br>";
+//two
+echo 50 % 50;
+echo "<br>";
+//three
+echo 70 % 60;
+echo "<br>";
+//four
+echo 90 % 180;
+echo "<br>";
+//five
+echo 160 % 80;
+echo "<br>";
+//six
+echo 200 % 700;
+echo "<br>";
+//seven
+echo 500 % 900;
+echo "<br>";
+//eight 
+echo 470 % 40;
+echo "<br>";
+//nine
+echo 560 % 430;
+echo "<br>";
+//ten
+echo 690 % 890;
+
+    ?>
